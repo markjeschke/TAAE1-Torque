6a1913dda15be51be847f715d297df5e05ec88dc
Mark Jeschke <markjeschke@marks-air.attlocal.net>
2016-08-23 15:33:30 -0700

    Initial Commit

---
+++ TAAE1-Torque/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TAAE1-Torque
+//
+//  Created by Mark Jeschke on 8/23/16.
+//  Copyright © 2016 Mark Jeschke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TAAE1-Torque/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TAAE1-Torque
+//
+//  Created by Mark Jeschke on 8/23/16.
+//  Copyright © 2016 Mark Jeschke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
